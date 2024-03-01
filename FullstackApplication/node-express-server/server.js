13f106261dc6799ed71c57d7f59db922b24703ac
Anitha Rajamuthu <34361796+AnithaRajamuthu@users.noreply.github.com>
2024-03-01 19:03:30 +0530

    Update server.js

--- FullstackApplication/node-express-server/server.js
+++ FullstackApplication/node-express-server/server.js
@@ -32,7 +32,7 @@ db.sequelize.sync()
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to tutorial backend application." });
 });
 
 require("./app/routes/turorial.routes")(app);
